156bb5a8cdcc9aaebb636b398b2cb19fed7c9d68
Adwait Kulkarni <65598707+AdwaitKulkarni58@users.noreply.github.com>
2021-05-25 21:33:07 +0530

    Updated main.c

--- Largest number in array/main.c
+++ Largest number in array/main.c
@@ -9,7 +9,7 @@ int main(void) {
   for (int i = 0; i < nums; i++){
     scanf("%d", &a[i]);
   }
-  for (int i = 1; i <= nums; i++){
+  for (int i = 0; i <= nums; i++){
     if (a[i] > maxNum){
       maxNum = a[i];
     }
